0855b0ee35ff1e709bcc97b252fa5d0dce0da516
Terminal123 <138257699+Dark-Ai-py@users.noreply.github.com>
2024-05-04 08:09:13 -0500

    installed daisy ui and tailwind

---
+++ vue-tailwind/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  
+</script>
+
+<template>
+  <div data-theme='nord' class="wrapper">
+    
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
